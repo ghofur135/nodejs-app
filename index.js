497064857ba2d6373a36195aa1a83923728eba43
ghofur135 <ghofur135@gmail.com>
2021-12-14 15:23:13 +0700

    add main page

--- index.js
+++ index.js
@@ -7,7 +7,9 @@
     app.get("/", function (req, res) {
      return res.send("Hello World via GitHub with TeamCity 2021 with VPS");
     });
-
+    app.get("/main", function (req, res) {
+      return res.send("Welcome to node js APP");
+     });
     // listen to port 7000 by default
     app.listen(process.env.PORT || 7000, () => {
       console.log("Server is running");
